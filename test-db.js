de8e029db6d8ee7b84200e22e4f024b95838c3a0
Komara11 <rahesakomara12@gmail.com>
2026-09-10 00:26:07 +0700

    chore: remove patch files from git index

--- test-db.js
+++
@@ -1,9 +0,0 @@
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
-async function main() {
-  const hero = await prisma.siteContent.findUnique({ where: { id: "hero" } });
-  console.log("HERO:", JSON.stringify(hero, null, 2));
-  const pkgs = await prisma.package.findMany({ orderBy: { sortOrder: 'asc' } });
-  console.log("PKGS:", JSON.stringify(pkgs.slice(0,2), null, 2));
-}
-main().catch(console.error).finally(() => prisma.$disconnect());
